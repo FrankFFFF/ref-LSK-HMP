torvalds/linux

commit df99953c42c67348f11cef42c420067e5f9a3239

cpuidle: governor: menu: don't use loadavg
get_loadavg doesn't work as intended.  According to the comments, it
should be returning an average over a few seconds, but it is actually
reading the instantaneous load.  It is almost always returning 0, but
can sometimes, depending on workload, spike very high into the hundreds
even when the average cpu load is under 10%.  Disable it for now.

Change-Id: I63ed100af1cf9463549939b8113ed83676db5f86
Signed-off-by: Colin Cross <ccross@android.com>

drivers/cpuidle/governors/menu.c
@@ -187,7 +187,12 @@ static inline int performance_multiplier(void)
 
 	/* for higher loadavg, we are more reluctant */
 
-	mult += 2 * get_loadavg();
+	/*
+	 * this doesn't work as intended - it is almost always 0, but can
+	 * sometimes, depending on workload, spike very high into the hundreds
+	 * even when the average cpu load is under 10%.
+	 */
+	/* mult += 2 * get_loadavg(); */
 
 	/* for IO wait tasks (per cpu!) we add 5x each */
 	mult += 10 * nr_iowait_cpu(smp_processor_id());

FrankFFFF/ref-LSK-HMP

commit 35c83b962efb04686ed4f1e4297c70214dfde873

cpuidle: governor: menu: comment out unused variable
Remove unused variable referenced from commented out code.

Fixes warning caused by commit df99953c42c6 ("cpuidle: governor:
menu: don't use loadavg")

Signed-off-by: Mark Hambleton <mark.hambleton@arm.com>
Signed-off-by: Jon Medhurst <tixy@linaro.org>

drivers/cpuidle/governors/menu.c
@@ -126,6 +126,7 @@ struct menu_device {
 #define LOAD_INT(x) ((x) >> FSHIFT)
 #define LOAD_FRAC(x) LOAD_INT(((x) & (FIXED_1-1)) * 100)
 
+#if 0
 static int get_loadavg(void)
 {
 	unsigned long this = this_cpu_load();
@@ -133,7 +134,7 @@ static int get_loadavg(void)
 
 	return LOAD_INT(this) * 10 + LOAD_FRAC(this) / 10;
 }
-
+#endif
 static inline int which_bucket(unsigned int duration)
 {
 	int bucket = 0;